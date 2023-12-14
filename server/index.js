e3a203c26a1de7e081f429e2bc329bd97c3d1e31
musthafa.id78 <musthafa.id78@gmail.com>
2023-12-14 16:09:05 +0530

    nodemailer setup finished

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import userRouter from './Router/userRouter.js';
 import postRouter from  './Router/postRouter.js'
 import { verifyToken } from "./middleware/auth.js";
 
-const  __filename = fileURLToPath(import.meta.url);
+const  __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
